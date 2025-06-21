1e400b3f1af800833f5930501be79cc088951cbb
PhuongKD <phuongkd160120@gmail.com>
2025-06-21 13:18:41 +0700

    edit animation

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,7 +1,6 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-declare var TweenMax: any;
+import { gsap } from 'gsap';
 
 @Component({
     selector: 'app-login',
@@ -9,407 +8,13 @@ declare var TweenMax: any;
     styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements AfterViewInit, OnDestroy {
-    // @ViewChild('loginEmailInput') email!: ElementRef;
-    // @ViewChild('loginEmailLabel') emailLabel!: ElementRef;
-    // @ViewChild('loginPasswordInput') password!: ElementRef;
-    // @ViewChild('loginPasswordLabel') passwordLabel!: ElementRef;
-    // @ViewChild('showPasswordCheck') showPasswordCheck!: ElementRef;
-    // @ViewChild('showPasswordToggleLabel') showPasswordToggleLabel!: ElementRef;
     @ViewChild('svgContainer') svgContainer!: ElementRef;
     @ViewChild('loginEmailInput') loginEmail!: ElementRef;
     @ViewChild('loginPasswordInput') loginPassword!: ElementRef;
     @ViewChild('loginEmailLabel') loginEmailLabel!: ElementRef;
     @ViewChild('showPasswordCheck') showPasswordCheck!: ElementRef;
-    @ViewChild('showPasswordToggle') showPasswordToggle!: ElementRef;
-    // @ViewChild('twoFingers') twoFingers!: ElementRef;
+    @ViewChild('showPasswordToggleLabel') showPasswordToggle!: ElementRef;
 
-    // @ViewChild('armL') armL!: ElementRef;
-    // @ViewChild('armR') armR!: ElementRef;
-    // @ViewChild('eyeL') eyeL!: ElementRef;
-    // @ViewChild('eyeR') eyeR!: ElementRef;
-    // @ViewChild('nose') nose!: ElementRef;
-    // @ViewChild('mouth') mouth!: ElementRef;
-    // @ViewChild('mouthBG') mouthBG!: ElementRef;
-    // @ViewChild('mouthSmallBG') mouthSmallBG!: ElementRef;
-    // @ViewChild('mouthMediumBG') mouthMediumBG!: ElementRef;
-    // @ViewChild('mouthLargeBG') mouthLargeBG!: ElementRef;
-    // @ViewChild('mouthMaskPath') mouthMaskPath!: ElementRef;
-    // @ViewChild('mouthOutline') mouthOutline!: ElementRef;
-    // @ViewChild('tooth') tooth!: ElementRef;
-    // @ViewChild('tongue') tongue!: ElementRef;
-    // @ViewChild('chin') chin!: ElementRef;
-    // @ViewChild('face') face!: ElementRef;
-    // @ViewChild('eyebrow') eyebrow!: ElementRef;
-    // @ViewChild('outerEarL') outerEarL!: ElementRef;
-    // @ViewChild('outerEarR') outerEarR!: ElementRef;
-    // @ViewChild('earHairL') earHairL!: ElementRef;
-    // @ViewChild('earHairR') earHairR!: ElementRef;
-    // @ViewChild('hair') hair!: ElementRef;
-    // @ViewChild('bodyBG') bodyBG!: ElementRef;
-    // @ViewChild('bodyBGchanged') bodyBGchanged!: ElementRef;
-
-    // private activeElement: string | null = null;
-    // private curEmailIndex: number = 0;
-    // private screenCenter: number = 0;
-    // private svgCoords: any;
-    // private emailCoords: { x: number; y: number} = { x: 0, y: 0};
-    // private emailScrollMax: number = 0;
-    // private readonly chinMin: number = 0.5;
-    // private dFromC: number = 0;
-    // private mouthStatus: string = 'small';
-    // private blinking: any;
-    // private eyeScale: number = 1;
-    // private eyesCovered: boolean = false;
-    // private showPasswordClicked: boolean = false;
-    // private eyeLCoords: { x: number; y: number} = { x: 0, y: 0};
-    // private eyeRCoords: { x: number; y: number} = { x: 0, y: 0};
-    // private noseCoords: { x: number; y: number} = { x: 0, y: 0};
-    // private mouthCoords: { x: number; y: number} = { x: 0, y: 0};
-
-    // private eyeLAngle: any;
-    // private eyeLX: any;
-    // private eyeLY: any;
-    // private eyeRAngle: any;
-    // private eyeRX: any;
-    // private eyeRY: any;
-    // private noseAngle: any;
-    // private noseX: any;
-    // private noseY: any;
-    // private mouthAngle: any;
-    // private mouthX: any;
-    // private mouthY: any;
-    // private mouthR: any;
-    // private chinX: any;
-    // private chinY: any;
-    // private chinS: any;
-    // private faceX: any;
-    // private faceY: any;
-    // private faceSkew: any;
-    // private eyebrowSkew: any;
-    // private outerEarX: any;
-    // private outerEarY: any;
-    // private hairX: any;
-    // private hairS: any;
-
-    // constructor() {}
-
-    // ngOnInit() {
-    //     // setTimeout(() => this.initLoginForm(), 0);
-    // }
-
-    // private initLoginForm(): void {
-    //     // this.svgCoords = this.getPosition(this.mySVG.nativeElement);
-    //     this.emailCoords = this.getPosition(this.email.nativeElement);
-    //     // this.screenCenter = this.svgCoords.x + this.mySVG.nativeElement.offsetWidth / 2;
-    //     // this.eyeLCoords = { x: this.svgCoords.x + 84, y: this.svgCoords.y + 76 };
-    //     // this.eyeRCoords = { x: this.svgCoords.x + 113, y: this.svgCoords.y + 76 };
-    //     // this.noseCoords = { x: this.svgCoords.x + 97, y: this.svgCoords.y + 81 };
-    //     // this.mouthCoords = { x: this.svgCoords.x + 100, y: this.svgCoords.y + 100 };
-    //     //
-    //     // // Set vị trí ban đầu cho cánh tay
-    //     // TweenMax.set(this.armL.nativeElement, {
-    //     //     x: -93,
-    //     //     y: 220,
-    //     //     rotation: 105,
-    //     //     transformOrigin: 'top left',
-    //     // });
-    //     // TweenMax.set(this.armR.nativeElement, {
-    //     //     x: -93,
-    //     //     y: 220,
-    //     //     rotation: -105,
-    //     //     transformOrigin: 'top right',
-    //     // });
-    //     //
-    //     // // Set thuộc tính ban đầu cho miệng
-    //     // TweenMax.set(this.mouth.nativeElement, {
-    //     //     transformOrigin: 'center center',
-    //     // });
-    //     //
-    //     // // Kích hoạt hiệu ứng nháy mắt
-    //     // this.startBlinking(5);
-    //     //
-    //     // // Xác định độ dài tối đa của input email
-    //     // this.emailScrollMax = this.email.nativeElement.scrollWidth;
-    //     //
-    //     // // Kiểm tra thiết bị di động
-    //     // if (this.isMobileDevice()) {
-    //     //     this.password.nativeElement.type = 'text';
-    //     //     this.showPasswordCheck.nativeElement.checked = true;
-    //     //     TweenMax.set(this.twoFingers.nativeElement, {
-    //     //         transformOrigin: 'bottom left',
-    //     //         rotation: 30,
-    //     //         x: -9,
-    //     //         y: -2,
-    //     //         ease: 'Power2.easeInOut',
-    //     //     });
-    //     // }
-    // }
-
-    // onEmailInput(): void {
-    //      this.email.nativeElement.parentElement.classList.add('focusWithText');
-    //     this.calculateFaceMove();
-    //     const value = this.email.nativeElement.value;
-    //     console.log(this.calculateFaceMove(), value)
-    //     // this.curEmailIndex = value.length;
-    //     //
-    //     // if (this.curEmailIndex > 0) {
-    //     //     if (this.mouthStatus == 'small') {
-    //     //         this.mouthStatus = 'medium';
-    //     //         TweenMax.to(
-    //     //             [
-    //     //                 this.mouthBG.nativeElement,
-    //     //                 this.mouthOutline.nativeElement,
-    //     //                 this.mouthMaskPath.nativeElement,
-    //     //             ],
-    //     //             1,
-    //     //             {
-    //     //                 morphSVG: this.mouthMediumBG.nativeElement,
-    //     //                 shapeIndex: 8,
-    //     //                 ease: 'Expo.easeOut',
-    //     //             },
-    //     //         );
-    //     //         // ... Thêm các animation khác tương tự
-    //     //     }
-    //     // }
-    // }
-
-    // // // Thêm các phương thức khác tương tự...
-    // //
-    // // private calculateFaceMove(): void {
-    // //     // Logic tính toán chuyển động khuôn mặt
-    // // }
-    // //
-    // // private getPosition(el: HTMLElement): { x: number; y: number } {
-    // //     let xPos = 0;
-    // //     let yPos = 0;
-    // //
-    // //     while (el) {
-    // //         if (el.tagName == 'BODY') {
-    // //             xPos +=
-    // //                 el.offsetLeft -
-    // //                 (document.documentElement.scrollLeft || document.body.scrollLeft) +
-    // //                 el.clientLeft;
-    // //             yPos +=
-    // //                 el.offsetTop -
-    // //                 (document.documentElement.scrollTop || document.body.scrollTop) +
-    // //                 el.clientTop;
-    // //         } else {
-    // //             xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
-    // //             yPos += el.offsetTop - el.scrollTop + el.clientTop;
-    // //         }
-    // //         el = el.offsetParent as HTMLElement;
-    // //     }
-    // //     return { x: xPos, y: yPos };
-    // // }
-    // //
-    // // private isMobileDevice(): boolean {
-    // //     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    // //         navigator.userAgent,
-    // //     );
-    // // }
-    // //
-    // // private startBlinking(delay: number): void {
-    // //     if (delay) {
-    // //         delay = this.getRandomInt(delay);
-    // //     } else {
-    // //         delay = 1;
-    // //     }
-    // //     this.blinking = TweenMax.to([this.eyeL.nativeElement, this.eyeR.nativeElement], 0.1, {
-    // //         delay: delay,
-    // //         scaleY: 0,
-    // //         yoyo: true,
-    // //         repeat: 1,
-    // //         transformOrigin: 'center center',
-    // //         onComplete: () => {
-    // //             this.startBlinking(12);
-    // //         },
-    // //     });
-    // // }
-    // //
-    // // private getRandomInt(max: number): number {
-    // //     return Math.floor(Math.random() * Math.floor(max));
-    // // }
-
-    // private calculateFaceMove() {
-    //     let carPos = this.email.nativeElement.selectionEnd;
-    //     const div = document.createElement('div');
-    //     const span = document.createElement('span');
-    //     let copyStyle = getComputedStyle(this.email.nativeElement);
-    //     let caretCoords = { x: 0, y: 0 };
-    //     if (carPos == null || carPos == 0) {
-    //         carPos = this.email.nativeElement.value.length;
-    //     }
-    //     [].forEach.call(copyStyle, function (prop) {
-    //         div.style[prop] = copyStyle[prop];
-    //     });
-    //     div.style.position = 'absolute';
-    //     document.body.appendChild(div);
-    //     div.textContent = this.email.nativeElement.value.substring(0, carPos);
-    //     span.textContent = this.email.nativeElement.value.substring(carPos) || '.';
-    //     div.appendChild(span);
-
-    //     if (this.email.nativeElement.scrollWidth <= this.emailScrollMax) {
-    //         caretCoords = this.getPosition(span);
-    //         this.dFromC = this.screenCenter - (caretCoords.x + this.emailCoords.x);
-    //         this.eyeLAngle = this.getAngle(
-    //             this.eyeLCoords.x,
-    //             this.eyeLCoords.y,
-    //             this.emailCoords.x + caretCoords.x,
-    //             this.emailCoords.y + 25,
-    //         );
-    //         this.eyeRAngle =  this.getAngle(
-    //             this.eyeRCoords.x,
-    //             this.eyeRCoords.y,
-    //             this.emailCoords.x + caretCoords.x,
-    //             this.emailCoords.y + 25,
-    //         );
-    //         this.noseAngle =  this.getAngle(
-    //             this.noseCoords.x,
-    //             this.noseCoords.y,
-    //             this.emailCoords.x + caretCoords.x,
-    //             this.emailCoords.y + 25,
-    //         );
-    //         this.mouthAngle =  this.getAngle(
-    //             this.mouthCoords.x,
-    //             this.mouthCoords.y,
-    //             this.emailCoords.x + caretCoords.x,
-    //             this.emailCoords.y + 25,
-    //         );
-    //     } else {
-    //         this.eyeLAngle = this.getAngle(
-    //             this.eyeLCoords.x,
-    //             this.eyeLCoords.y,
-    //             this.emailCoords.x + this.emailScrollMax,
-    //             this.emailCoords.y + 25,
-    //         );
-    //         this.eyeRAngle = this.getAngle(
-    //             this.eyeRCoords.x,
-    //           this.eyeRCoords.y,
-    //           this.emailCoords.x + this.emailScrollMax,
-    //           this.emailCoords.y + 25,
-    //         );
-    //         this.noseAngle = this.getAngle(
-    //           this.noseCoords.x,
-    //           this.noseCoords.y,
-    //           this.emailCoords.x + this.emailScrollMax,
-    //           this.emailCoords.y + 25,
-    //         );
-    //         this.mouthAngle = this.getAngle(
-    //           this.mouthCoords.x,
-    //           this.mouthCoords.y,
-    //           this.emailCoords.x + this.emailScrollMax,
-    //           this.emailCoords.y + 25,
-    //         );
-    //     }
-
-    //   this.eyeLX = Math.cos(this.eyeLAngle) * 20;
-    //   this.eyeLY = Math.sin(this.eyeLAngle) * 10;
-    //   this.eyeRX = Math.cos(this.eyeRAngle) * 20;
-    //   this.eyeRY = Math.sin(this.eyeRAngle) * 10;
-    //   this.noseX = Math.cos(this.noseAngle) * 23;
-    //   this.noseY = Math.sin(this.noseAngle) * 10;
-    //   this.mouthX = Math.cos(this.mouthAngle) * 23;
-    //   this.mouthY = Math.sin(this.mouthAngle) * 10;
-    //   this.mouthR = Math.cos(this.mouthAngle) * 6;
-    //   this.chinX = this.mouthX * 0.8;
-    //   this.chinY = this.mouthY * 0.5;
-    //   this.chinS = 1 - (this.dFromC * 0.15) / 100;
-    //     if (this.chinS > 1) {
-    //       this.chinS = 1 - (this.chinS - 1);
-    //         if (this.chinS < this.chinMin) {
-    //           this.chinS = this.chinMin;
-    //         }
-    //     }
-    //   this.faceX = this.mouthX * 0.3;
-    //   this.faceY = this.mouthY * 0.4;
-    //   this.faceSkew = Math.cos(this.mouthAngle) * 5;
-    //   this.eyebrowSkew = Math.cos(this.mouthAngle) * 25;
-    //   this.outerEarX = Math.cos(this.mouthAngle) * 4;
-    //   this.outerEarY = Math.cos(this.mouthAngle) * 5;
-    //   this.hairX = Math.cos(this.mouthAngle) * 6;
-    //   this.hairS = 1.2;
-
-    //     TweenMax.to(this.eyeL, 1, { x: -this.eyeLX, y: -this.eyeLY, ease: Expo.easeOut });
-    //     TweenMax.to(this.eyeR, 1, { x: -this.eyeRX, y: -this.eyeRY, ease: Expo.easeOut });
-    //     TweenMax.to(this.nose, 1, {
-    //         x: -this.noseX,
-    //         y: -this.noseY,
-    //         rotation: this.mouthR,
-    //         transformOrigin: 'center center',
-    //         ease: Expo.easeOut,
-    //     });
-    //     TweenMax.to(this.mouth, 1, {
-    //         x: -this.mouthX,
-    //         y: -this.mouthY,
-    //         rotation: this.mouthR,
-    //         transformOrigin: 'center center',
-    //         ease: Expo.easeOut,
-    //     });
-    //     TweenMax.to(this.chin, 1, { x: -this.chinX, y: -this.chinY, scaleY: this.chinS, ease: Expo.easeOut });
-    //     TweenMax.to(this.face, 1, {
-    //         x: -this.faceX,
-    //         y: -this.faceY,
-    //         skewX: -this.faceSkew,
-    //         transformOrigin: 'center top',
-    //         ease: Expo.easeOut,
-    //     });
-    //     TweenMax.to(this.eyebrow, 1, {
-    //         x: -this.faceX,
-    //         y: -this.faceY,
-    //         skewX: -this.eyebrowSkew,
-    //         transformOrigin: 'center top',
-    //         ease: Expo.easeOut,
-    //     });
-    //     TweenMax.to(this.outerEarL, 1, { x: this.outerEarX, y: -this.outerEarY, ease: Expo.easeOut });
-    //     TweenMax.to(this.outerEarR, 1, { x: this.outerEarX, y: this.outerEarY, ease: Expo.easeOut });
-    //     TweenMax.to(this.earHairL, 1, { x: -this.outerEarX, y: -this.outerEarY, ease: Expo.easeOut });
-    //     TweenMax.to(this.earHairR, 1, { x: -this.outerEarX, y: this.outerEarY, ease: Expo.easeOut });
-    //     TweenMax.to(this.hair, 1, {
-    //         x: this.hairX,
-    //         scaleY: this.hairS,
-    //         transformOrigin: 'center bottom',
-    //         ease: Expo.easeOut,
-    //     });
-
-    //     document.body.removeChild(div);
-    // }
-
-    // private getPosition(el: any) {
-    //     let xPos = 0;
-    //     let yPos = 0;
-
-    //     while (el) {
-    //         if (el.nativeElement.tagName == 'BODY') {
-    //             // deal with browser quirks with body/window/document and page scroll
-    //             let xScroll = el.nativeElement.scrollLeft || document.documentElement.scrollLeft;
-    //             let yScroll = el.nativeElement.scrollTop || document.documentElement.scrollTop;
-
-    //             xPos += el.nativeElement.offsetLeft - xScroll + el.nativeElement.clientLeft;
-    //             yPos += el.nativeElement.offsetTop - yScroll + el.nativeElement.clientTop;
-    //         } else {
-    //             // for all other non-BODY elements
-    //             xPos +=
-    //                 el.nativeElement.offsetLeft -
-    //                 el.nativeElement.scrollLeft +
-    //                 el.nativeElement.clientLeft;
-    //             yPos +=
-    //                 el.nativeElement.offsetTop -
-    //                 el.nativeElement.scrollTop +
-    //                 el.nativeElement.clientTop;
-    //         }
-
-    //         el = el.nativeElement.offsetParent;
-    //     }
-
-    //     return {
-    //         x: xPos,
-    //         y: yPos,
-    //     };
-    // }
-
-    // private getAngle(x1: any, y1: any, x2: any, y2: any) {
-    //     return Math.atan2(y1 - y2, x1 - x2);
-    // }
 
     loginForm: FormGroup;
     showPassword = false;
@@ -549,6 +154,7 @@ export class LoginComponent implements AfterViewInit, OnDestroy {
     onEmailFocus(event: any) {
         this.activeElement = 'email';
         this.emailFocused = true;
+        this.loginEmailLabel.nativeElement.parentElement?.classList.add('focusWithText');
         this.onEmailInput(event);
     }
 
@@ -560,6 +166,9 @@ export class LoginComponent implements AfterViewInit, OnDestroy {
             if (this.activeElement !== 'email') {
                 this.resetFace();
             }
+            if (event.target.value == "") {
+              this.loginEmailLabel.nativeElement.parentElement?.classList.remove('focusWithText');
+            }
         }, 100);
     }
 
@@ -567,6 +176,8 @@ export class LoginComponent implements AfterViewInit, OnDestroy {
         this.calculateFaceMove(event);
         const value = event.target.value;
         this.curEmailIndex = value.length;
+
+        this.loginEmailLabel.nativeElement.parentElement?.classList.add('focusWithText');
 
         // Email validation effects
         if (this.curEmailIndex > 0) {
